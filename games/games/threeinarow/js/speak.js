535b626bee0981eb210ba71a47c0d93c601f7a60
ravindragameshub <ravindragameshub@gmail.com>
2025-10-18 23:07:29 +0530

    Add ledder board displayed

--- games/games/threeinarow/js/speak.js
+++
@@ -1,9 +0,0 @@
-// para to speeach in english
-export function textToSpeechEng(text) {
-    let speechSynthesis = window.speechSynthesis;
-    let utterance = new SpeechSynthesisUtterance();
-    utterance.lang = "en-US";
-    utterance.voice = speechSynthesis.getVoices().find(voice => voice.lang === 'en-US');
-    utterance.text = text;
-    speechSynthesis.speak(utterance);
-  }
