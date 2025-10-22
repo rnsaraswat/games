9c867ef1e3c321692917ad5e98fc97949d30e7d4
ravindragameshub <ravindragameshub@gmail.com>
2025-10-22 21:04:20 +0530

    login testing

--- leaderboard/config.js
+++ leaderboard/config.js
@@ -1,4 +1,4 @@
-export const SUPABASE_URL = 'https://bkhoexvgorxzgdujofar.supabase.co';
+export const SUPABASE_URL = 'https://bkhoexvgorxzgdujofar.supabase.co/functions/v1/submit-score';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJraG9leHZnb3J4emdkdWpvZmFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MTE3NDgsImV4cCI6MjA3NjA4Nzc0OH0.DG1jB5GDBJAtfOsJF0KjO8luVVTLTgx6MlZIvj_v7IQ';
 // const SUPABASE_EDGE_FUNCTION_URL = "https://bkhoexvgorxzgdujofar.supabase.co/functions/v1/submit-score";
 
